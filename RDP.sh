7af8a2d4580c49f64bd5b34c21861ef821b80994
explorerfaketimepass <50465314+explorerfaketimepass@users.noreply.github.com>
2021-01-05 17:39:25 +0530

    Update RDP.sh
    root password 0000

--- RDP.sh
+++ RDP.sh
@@ -29,6 +29,7 @@ printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
     printf "\n\nUpgrade Completed " >&2
+    printf "\n\nRoot password is 0000 " >&2
 else
     printf "\n\nError Occured " >&2
 fi
